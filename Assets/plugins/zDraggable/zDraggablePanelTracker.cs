ee6bbfaf6cc1eec0dff9677b16753037c7b8f69d
zambari@gmail.com <zambari@gmail.com>
2017-05-07 01:24:37 +0200

    unitypackage updated

--- Assets/plugins/zDraggable/zDraggablePanelTracker.cs
+++ Assets/plugins/zDraggable/zDraggablePanelTracker.cs
@@ -1,5 +1,6 @@
 ﻿//z2k17
 
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 //using UnityEngine.UI.Extensions;
@@ -9,32 +10,53 @@ using UnityEngine.UI;
 public class zDraggablePanelTracker : MonoBehaviour
 {
 
-    public Toggle templateObject;
 
+    public Toggle templateObject;
+    List<zDraggable> dragList;
+
+    List<Toggle> toggleList;
+    public Toggle toggleAll;
     void OnValidate()
     {
         if (templateObject == null) templateObject = GetComponentInChildren<Toggle>();
         if (templateObject != null) templateObject.gameObject.SetActive(false);
+
     }
 
     void Awake()
     {
-       
+        toggleList = new List<Toggle>();
         if (zDraggable.draggableList != null)
             for (int i = 0; i < zDraggable.draggableList.Count; i++)
                 newPanel(zDraggable.draggableList[i]);
 
-    zDraggable.newPanel += newPanel;
+        zDraggable.newPanel += newPanel;
+        if (toggleAll != null)
+        {
+            toggleAll.onValueChanged.AddListener(toggleAllPanels);
+        }
+    }
+    public void toggleAllPanels(bool onoff)
+    {
+        if (zDraggable.draggableList != null)
+            for (int i = 0; i < zDraggable.draggableList.Count; i++)
+                zDraggable.draggableList[i].gameObject.SetActive(onoff);
+        for (int i = 0; i < toggleList.Count; i++) toggleList[i].isOn = onoff;
     }
     void newPanel(zDraggable panel)
     {
-        Toggle t = Instantiate(templateObject, transform);
+        if (dragList == null) dragList = new List<zDraggable>();
+        if (dragList.Contains(panel)) return;
+        dragList.Add(panel);
+        Toggle t = Instantiate(templateObject, templateObject.transform.parent);
+        toggleList.Add(t);
         t.gameObject.SetActive(true);
+        t.name = panel.name;
         t.onValueChanged.AddListener((x) => panel.gameObject.SetActive(x));
 
 
-        Text text=t.GetComponentInChildren<Text>();
-        if (text!=null) text.text=panel.name;
+        Text text = t.GetComponentInChildren<Text>();
+        if (text != null) text.text = panel.name;
 
     }
 
